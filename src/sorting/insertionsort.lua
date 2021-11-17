4b0fdae6cfde2117602fefc3311e17a0d173e523
Lars Mueller <appgurulars@gmx.de>
2021-11-17 21:28:39 +0100

    Add Insertionsort

---
+++ src/sorting/insertionsort.lua
@@ -0,0 +1,25 @@
+--! Insertionsort has quadratic time complexity and should not be used in practice
+return function(
+	-- list to be sorted in-place
+	list,
+	-- function(a, b) -> truthy value if a < b
+	less_than
+)
+	less_than = less_than or function(a, b)
+		return a < b
+	end
+	for index = 2, #list do
+		local value = list[index]
+		-- Even if a binary search was used to determine the insertion index,
+		-- time complexity would remain quadratic due to the series of swaps required for insertion
+		local insertion_index = 0
+		repeat
+			insertion_index = insertion_index + 1
+		until less_than(value, list[insertion_index]) or insertion_index >= #list
+		-- Shift all elements - starting at the insertion index - up by one
+		for shift_index = index - 1, insertion_index, -1 do
+			list[shift_index + 1] = list[shift_index]
+		end
+		list[insertion_index] = value
+	end
+end
