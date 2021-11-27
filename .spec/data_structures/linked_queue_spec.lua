fc58981416fe2d4e10dadee7ad190e80f0a581fc
Lars Mueller <appgurulars@gmx.de>
2021-11-27 14:17:20 +0100

    Add linked queue

---
+++ .spec/data_structures/linked_queue_spec.lua
@@ -0,0 +1,15 @@
+describe("Linked queue", function()
+	local linked_queue = require("data_structures.linked_queue")
+	it("should work", function()
+		local queue = linked_queue.new()
+		assert.truthy(queue:empty())
+		for i = 1, 10 do
+			queue:push(i)
+		end
+		for i = 1, 10 do
+			assert.equals(i, queue:top())
+			assert.equals(i, queue:pop())
+		end
+		assert.truthy(queue:empty())
+	end)
+end)
---
+++ src/data_structures/linked_queue.lua
@@ -0,0 +1,41 @@
+local linked_queue = {}
+local metatable = { __index = linked_queue }
+
+function linked_queue.new()
+	return setmetatable({}, metatable)
+end
+
+function linked_queue:empty()
+	return not self._top
+end
+
+function linked_queue:push(value)
+	if self:empty() then
+		self._top = { value = value }
+		self._bottom = self._top
+	else
+		self._bottom.below = { value = value }
+		self._bottom = self._bottom.below
+	end
+end
+
+function linked_queue:top()
+	if self:empty() then
+		return
+	end
+	return self._top.value
+end
+
+function linked_queue:pop()
+	if self:empty() then
+		return
+	end
+	local top_element = self:top()
+	self._top = self._top.below
+	if self:empty() then -- List became empty through popping
+		self._bottom = nil
+	end
+	return top_element
+end
+
+return linked_queue
