9eba03cd21561ae56690fd882990be02ef3333ee
Lars Mueller <appgurulars@gmx.de>
2022-02-14 00:54:29 +0100

    Make searches comparator-based

--- src/searches/binary_search.lua
+++ src/searches/binary_search.lua
@@ -1,20 +1,24 @@
 return function(
 	list,
 	-- Value to be searched
-	value
+	value,
+	-- Comparator
+	less_than
 )
+	less_than = less_than or function(a, b)
+		return a < b
+	end
 	local min, max = 1, #list
 	while min <= max do
 		local pivot = min + math.floor((max - min) / 2)
 		local element = list[pivot]
-		if value == element then
+		if less_than(value, element) then
+			max = pivot - 1
+		elseif less_than(element, value) then
+			min = pivot + 1
+		else -- Neither smaller nor larger => must be equal
 			-- Index if found
 			return pivot
-		elseif value > element then
-			min = pivot + 1
-		else
-			assert(value < element, "invalid order operators for binary search")
-			max = pivot - 1
 		end
 	end
 	-- Negative insertion index if not found
--- src/searches/linear_search.lua
+++ src/searches/linear_search.lua
@@ -3,15 +3,22 @@ return function(
 	-- Value to be searched
 	value,
 	-- Whether the list is sorted
-	sorted
+	sorted,
+	-- Comparator
+	less_than
 )
+	less_than = less_than or function(a, b)
+		return a < b
+	end
 	for index, element in ipairs(list) do
-		if element == value then
+		if less_than(value, element) then
+			if sorted then
+				-- Negative insertion index if the list is sorted and the value was not found
+				return -index
+			end
+		elseif not less_than(element, value) then -- must be equal
 			-- Index if found
 			return index
-		elseif element > value and sorted then
-			-- Negative insertion index if the list is sorted and the value was not found
-			return -index
 		end
 	end
 	-- Negative insertion index after the last element
