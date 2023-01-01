15d8a7830f485681eb89305b99ecbdbb148a995b
Lars Mueller <appgurulars@gmx.de>
2023-01-01 14:44:18 +0100

    Add Quickselect

---
+++ .spec/sorting/quickselect_spec.lua
@@ -0,0 +1,114 @@
+describe("Quickselect", function()
+	local build_quickselect = require("sorting.quickselect")
+	local function test_quickselect(quickselect, n)
+		local function test_order(descending)
+			local less_than
+			if descending then
+				less_than = function(a, b)
+					return a > b
+				end
+			end
+			-- Test that Quickselect correctly handles an (ascendingly) sorted array
+			local function test_sorted(arr)
+				local copy = { unpack(arr) }
+				for i = 1, #arr do
+					local expected = i
+					if descending then
+						expected = #arr - i + 1 -- from end (reverse order)
+					end
+					-- Expect the correct value; only call Quickselect a copy of the array
+					assert.equal(arr[expected], copy[quickselect(copy, i, less_than)])
+				end
+			end
+			-- Test a permutation from 1 to n
+			local function test_permutation(arr)
+				for i = 1, #arr do
+					local expected = i
+					if descending then
+						expected = #arr - i + 1 -- from end (reverse order)
+					end
+					assert.equal(expected, arr[quickselect(arr, i, less_than)])
+				end
+			end
+			describe("selected", function()
+				it("out of bound indices", function()
+					assert.has_error(function()
+						quickselect({}, 1, less_than)
+					end)
+					assert.has_error(function()
+						quickselect({}, 0, less_than)
+					end)
+					assert.has_error(function()
+						quickselect({ 1, 2, 3 }, 0, less_than)
+					end)
+					assert.has_error(function()
+						quickselect({ 1, 2, 3 }, 4, less_than)
+					end)
+				end)
+				it("edge cases", function()
+					test_sorted({ 42 })
+					test_permutation({ 1 })
+				end)
+				it("sorted arrays", function()
+					test_sorted({ "aa", "bb", "cb", "cc" })
+					test_sorted({ 1, 3, 5, 9, 42, 101 })
+				end)
+				it("permutations", function()
+					test_permutation({ 5, 4, 1, 2, 3 })
+				end)
+			end)
+			describe("random", function()
+				it("sorted arrays", function()
+					for _ = 1, n do
+						-- Test a sorted array of random values from 1 to n
+						local len = math.random(n)
+						local arr = {}
+						for i = 1, len do
+							arr[i] = math.random(n)
+						end
+						table.sort(arr)
+						test_sorted(arr)
+					end
+				end)
+				it("permutations", function()
+					local shuffle = require("random.shuffle")
+					for _ = 1, n do
+						-- Test a random permutation of 1 to n
+						local len = math.random(n)
+						local arr = {}
+						for i = 1, len do
+							arr[i] = i
+						end
+						shuffle(arr)
+						test_permutation(arr)
+					end
+				end)
+			end)
+		end
+		describe("ascending (default)", function()
+			test_order(false)
+		end)
+		describe("descending", function()
+			test_order(true)
+		end)
+	end
+	describe("default pivot picking strategy (random)", function()
+		test_quickselect(build_quickselect(), 100)
+	end)
+	describe("supports different pivot picking strategies", function()
+		local function test_strategy(name, choose_pivot)
+			describe("select " .. name, function()
+				test_quickselect(build_quickselect(choose_pivot), 20)
+			end)
+		end
+		test_strategy("first", function(from, _)
+			return from
+		end)
+		test_strategy("last", function(_, to)
+			return to
+		end)
+		test_strategy("mid", function(from, to)
+			return math.floor((from + to) / 2)
+		end)
+	end)
+end)
---
+++ src/sorting/quickselect.lua
@@ -0,0 +1,47 @@
+local three_way_partition = require("sorting.three_way_partition")
+
+-- Builder for a Quickselect function given a pivot choosing strategy
+return function(
+	choose_pivot -- `function(from, to) return pivot_idx` where from <= pivot_idx <= to; defaults to `math.random`
+)
+	choose_pivot = choose_pivot or math.random
+	-- Actual Quickselect function
+	return function(
+		-- unsorted table to select from; is permuted
+		arr,
+		-- index of the value to select if the table were sorted
+		sort_idx,
+		-- optional, defaults to `function(a, b) return a < b end`
+		less_than
+	)
+		less_than = less_than or function(a, b)
+			return a < b
+		end
+		assert(sort_idx >= 1 and sort_idx <= #arr and sort_idx == math.floor(sort_idx), "invalid index")
+		local function quickselect(from, to, sort_idx) -- luacheck: ignore
+			if from == to then -- single element
+				assert(sort_idx == 1)
+				return from
+			end
+			-- Partition slice into three parts:
+			-- 1. Values smaller than the pivot from `from` to `smaller_to`
+			-- 2. Values equal to the pivot from `smaller_to + 1` to `larger_from - 1`
+			-- 3. Values larger than the pivot from `larger_from` to `to`
+			local pivot_val = arr[choose_pivot(from, to)]
+			local smaller_to, larger_from = three_way_partition(arr, from, to, pivot_val, less_than)
+
+			local abs_idx = sort_idx + from - 1 -- absolute index: offset by left bound
+			-- Value at the index must be...
+			if abs_idx <= smaller_to then -- ... in the first part
+				return quickselect(from, smaller_to, sort_idx)
+			end
+			if abs_idx < larger_from then -- ... in the second part (equal values), return
+				return abs_idx
+			end
+			-- ... in the third part
+			local leq_count = larger_from - from
+			return quickselect(larger_from, to, sort_idx - leq_count)
+		end
+		return quickselect(1, #arr, sort_idx) -- index of the value in the array after the array has been permuted
+	end
+end
