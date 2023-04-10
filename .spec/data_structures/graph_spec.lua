293d815c75ee8df8794574c5577b7287abb21247
Lars Mueller <appgurulars@gmx.de>
2023-04-10 16:43:10 +0200

    Add directed graph data structure
    Includes traversals, topological sorting, shortest path (both single source and all pairs) and maximum flow algorithms

---
+++ .spec/data_structures/graph_spec.lua
@@ -0,0 +1,440 @@
+describe("Graph", function()
+	local graph = require("data_structures.graph")
+	local shuffle = require("random.shuffle")
+	local function create_n_nodes(n)
+		local g = graph.new()
+		for i = 1, n do
+			g:add_node(i)
+		end
+		return g
+	end
+	-- Helpers to create random graphs
+	local function random_n_nodes()
+		local n = math.random(30)
+		return create_n_nodes(n), n
+	end
+	local function random_directed_cyclic_graph()
+		local g, n = random_n_nodes()
+		local cyc_len = math.random(n)
+		-- Set up the cycle
+		for v = 2, cyc_len do
+			g:add_edge(v - 1, v)
+		end
+		g:add_edge(cyc_len, 1)
+		-- Now add random edges (may add more cycles)
+		for _ = 1, math.random(n ^ 2) do
+			g:add_edge(math.random(n), math.random(n))
+		end
+		return g, n
+	end
+	local function random_directed_acyclic_graph(random_weight)
+		random_weight = random_weight or function()
+			return true
+		end
+		local g = graph.new()
+		-- Add nodes
+		local n = math.random(2, 30)
+		local nodes = {}
+		for i = 1, n do
+			g:add_node(i)
+			nodes[i] = i
+		end
+		shuffle(nodes) -- pick a random topological ordering
+		-- Now add random edges which do not conflict with the topological ordering
+		for _ = 1, math.random(n ^ 2) do
+			local from = math.random(n - 1)
+			local to = math.random(from + 1, n)
+			g:set_weight(from, to, random_weight())
+		end
+		return g, n
+	end
+	local function random_graph_pos_weights()
+		local g, n = random_n_nodes()
+		for _ = 1, math.random(n ^ 2) do
+			g:set_weight(math.random(n), math.random(n), math.random(30))
+		end
+		return g, n
+	end
+	local function random_graph_with_shortest_path(negative_weights)
+		local max_weight_abs = 10
+		local g, n = random_n_nodes()
+		local dist, predec = { [1] = 0 }, {}
+		for node = 1, n - 1 do
+			local weight = negative_weights and math.random(-max_weight_abs, max_weight_abs)
+				or math.random(max_weight_abs)
+			g:set_weight(node, node + 1, weight)
+			dist[node + 1], predec[node + 1] = dist[node] + weight, node
+		end
+		-- Add edges which are longer than the total length of the path
+		local min_len = (negative_weights and max_weight_abs * n or dist[n]) + 1 -- make negative weight cycles impossible
+		for _ = 1, math.random(n ^ 2) do
+			local from, to = math.random(n), math.random(n)
+			if from + 1 ~= to then -- don't break the existing path
+				g:set_weight(from, to, math.random(min_len, 5 * min_len))
+			end
+		end
+		return g, dist, predec
+	end
+	it("construction, getting & setting nodes & edges", function()
+		local g = graph.new()
+		local expected_nodes, expected_weights = {}, {}
+		for i = 100, 1, -1 do
+			assert.equal(false, g:has_node(i))
+			g:add_node(i)
+			assert.equal(true, g:has_node(i))
+			expected_nodes[i] = true
+			expected_weights[i] = {}
+			for j = i, 100 do
+				local weight = math.random()
+				g:set_weight(i, j, weight)
+				expected_weights[i][j] = weight
+			end
+		end
+
+		for node in g:nodes() do
+			assert(expected_nodes[node]) -- node must be expected
+			expected_nodes[node] = nil -- node may not be seen again
+			-- Copy the expected neighbors
+			local expected_neighbors = {}
+			for neighbor, weight in pairs(expected_weights[node]) do
+				expected_neighbors[neighbor] = weight
+			end
+			-- Go over neighbors
+			for neighbor, weight in g:neighbors(node) do
+				assert.equal(expected_neighbors[neighbor], weight)
+				expected_neighbors[neighbor] = nil -- neighbor may not be seen again
+			end
+			-- Each neighbor should have been seen
+			assert.equal(nil, next(expected_neighbors))
+		end
+		assert.equal(nil, next(expected_nodes)) -- all nodes must have been seen
+
+		for from, to, weight in g:edges() do
+			assert.equal(true, g:has_node(from))
+			assert.equal(true, g:has_node(to))
+			assert.equal(weight, g:get_weight(from, to))
+			assert.equal(expected_weights[from][to], weight) -- weight must be as expected
+			expected_weights[from][to] = nil -- edge may not be seen again
+		end
+
+		-- All edges must have been seen
+		for _, neighbors in pairs(expected_weights) do
+			assert.equal(nil, next(neighbors))
+		end
+	end)
+	it("copy", function()
+		local g = graph.new({ a = { b = 42 }, b = {} })
+		assert.same(g, g:copy())
+	end)
+	local function traversal_common(fname)
+		local function visits_all_nodes(g)
+			local expected_nodes = {}
+			for node in g:nodes() do
+				expected_nodes[node] = true
+			end
+			for node in g[fname](g) do
+				assert(expected_nodes[node])
+				expected_nodes[node] = nil
+			end
+			assert.equal(nil, next(expected_nodes))
+		end
+		it("iterates the connected component if given a root", function()
+			local n = 42
+			local g = create_n_nodes(2 * n)
+			for node = 1, n - 1 do
+				g:add_edge(node, node + 1)
+			end
+			local expected_node = 0
+			for node, depth in g[fname](g, 1) do
+				expected_node = expected_node + 1
+				assert.equal(expected_node, node)
+				assert.equal(expected_node, depth + 1)
+			end
+			assert.equal(n, expected_node)
+		end)
+		it("handles chains correctly", function()
+			local n = 42
+			local g = create_n_nodes(n)
+			for i = 1, n - 1 do
+				g:add_edge(i, i + 1)
+			end
+			local expected_node = 0
+			for node, depth in g[fname](g) do
+				expected_node = expected_node + 1
+				assert.equal(expected_node, node)
+				assert.equal(expected_node, depth + 1)
+			end
+			assert.equal(n, expected_node)
+		end)
+		describe("visits all nodes", function()
+			it("for cyclic graphs", function()
+				visits_all_nodes(random_directed_cyclic_graph())
+			end)
+			it("for acyclic graphs", function()
+				visits_all_nodes(random_directed_acyclic_graph())
+			end)
+		end)
+	end
+	describe("breadth-first traversal", function()
+		traversal_common("nodes_breadth_first")
+	end)
+	describe("depth-first traversal", function()
+		traversal_common("nodes_depth_first")
+	end)
+	describe("cycle detection", function()
+		it("works on random cyclic graphs", function()
+			for _ = 1, 10 do
+				assert.equal(true, random_directed_cyclic_graph():has_cycle())
+			end
+		end)
+		it("works on random acyclic graphs", function()
+			for _ = 1, 10 do
+				assert.equal(false, random_directed_acyclic_graph():has_cycle())
+			end
+		end)
+	end)
+	describe("topological sort", function()
+		it("errors on cyclic graphs", function()
+			for _ = 1, 10 do
+				assert.has_error(function()
+					random_directed_cyclic_graph():nodes_topological_order()
+				end)
+			end
+		end)
+		it("returns a valid ordering on acyclic graphs", function()
+			for _ = 1, 10 do
+				local g = random_directed_acyclic_graph()
+				local ranks = {}
+				local i = 0
+				for node in g:nodes_topological_order() do
+					i = i + 1
+					ranks[node] = i
+				end
+				for from, to in g:edges() do
+					assert(ranks[from] < ranks[to])
+				end
+			end
+		end)
+	end)
+	local function test_sssp_limited(name, negative_weights)
+		it("selected graph", negative_weights and function()
+			-- This is an example where the invariant of Dijkstra's algorithm would be broken
+			local g = graph.new({
+				[1] = { [2] = 1, [3] = 3 },
+				[2] = { [4] = 1 },
+				[3] = { [2] = -3 },
+				[4] = {},
+			})
+			assert.same(
+				{
+					{
+						[1] = 0,
+						[2] = 0,
+						[3] = 3,
+						[4] = 1,
+					},
+					{
+						[2] = 3,
+						[3] = 1,
+						[4] = 2,
+					},
+				},
+				{ g[name](g, 1) }
+			)
+		end or function()
+			local g = graph.new({
+				[1] = { [1] = 1, [2] = 2, [4] = 3, [5] = 9 },
+				[2] = { [1] = 2, [3] = 4, [4] = 2 },
+				[3] = {},
+				[4] = { [2] = 5, [5] = 1 },
+				[5] = { [3] = 1, [6] = 1 },
+				[6] = { [2] = 1, [5] = 6, [4] = 3 },
+			})
+			assert.same(
+				{
+					{
+						[1] = 0,
+						[2] = 2,
+						[3] = 5,
+						[4] = 3,
+						[5] = 4,
+						[6] = 5,
+					},
+					{
+						[2] = 1,
+						[3] = 5,
+						[4] = 1,
+						[5] = 4,
+						[6] = 5,
+					},
+				},
+				{ g[name](g, 1) }
+			)
+		end)
+		it("random graphs", function()
+			for _ = 1, 10 do
+				local g, dist, predec = random_graph_with_shortest_path(negative_weights)
+				assert.same({ dist, predec }, { g[name](g, 1) })
+			end
+		end)
+	end
+	describe("SSSP", function()
+		describe("Dijkstra", function()
+			test_sssp_limited("sssp_dijkstra", false)
+			it("errors on negative weights", function()
+				assert.has_error(function()
+					graph.new({ [1] = { [2] = -1 }, [2] = {} }):sssp_dijkstra(1)
+				end)
+			end)
+		end)
+		local function test_sssp(name)
+			describe("supports positive weights", function()
+				test_sssp_limited(name, false)
+			end)
+			describe("supports negative weights", function()
+				test_sssp_limited(name, true)
+			end)
+			it("errors on negative weight cycles", function()
+				assert.has_error(function()
+					local g = graph.new({ [1] = { [2] = -1 }, [2] = { [1] = -1 } })
+					g[name](g, 1)
+				end)
+			end)
+		end
+		describe("Bellman-Ford", function()
+			test_sssp("sssp_bellman_ford")
+		end)
+		describe("Dispatcher", function()
+			test_sssp("sssp")
+		end)
+	end)
+	-- Checks that the implicit shortest path tree given as predecessor links matches the distances
+	local function check_shortest_path_tree(g, source, dist, predec)
+		local checked = {}
+		assert.equal(0, dist[source])
+		checked[source] = true
+		local function check(to)
+			if checked[to] then
+				return
+			end
+			local from = predec[to]
+			assert.equal(dist[to], g:get_weight(from, to) + dist[from])
+			check(from)
+			checked[to] = true
+		end
+		for node in pairs(predec) do
+			check(node)
+		end
+		for node in pairs(dist) do
+			assert(checked[node])
+		end
+	end
+	local function check_shortest_path_forest(g, dists, predecs)
+		for source, dist in pairs(dists) do
+			check_shortest_path_tree(g, source, dist, predecs[source])
+		end
+		return dists -- return the distances for convenience
+	end
+	local function test_apsp_pos_weights(name)
+		it("selected graph (positive weights)", function()
+			local g = graph.new({
+				[1] = { [4] = 2 },
+				[2] = { [1] = 3, [3] = 7 },
+				[3] = {},
+				[4] = { [2] = 5 },
+			})
+			assert.same({
+				{
+					[1] = { [1] = 0, [2] = 7, [3] = 14, [4] = 2 },
+					[2] = { [1] = 3, [2] = 0, [3] = 7, [4] = 5 },
+					[3] = { [3] = 0 },
+					[4] = { [1] = 8, [2] = 5, [3] = 12, [4] = 0 },
+				},
+				{
+					[1] = { [2] = 4, [3] = 2, [4] = 1 },
+					[2] = { [1] = 2, [3] = 2, [4] = 1 },
+					[3] = {},
+					[4] = { [1] = 2, [2] = 4, [3] = 2 },
+				},
+			}, { g[name](g) })
+		end)
+	end
+	local function test_apsp(name)
+		it("selected graph (negative weights)", function()
+			local g = graph.new({
+				[1] = { [2] = 5, [3] = 2 },
+				[2] = { [3] = -2, [4] = 3 },
+				[3] = { [1] = -1 },
+				[4] = { [1] = 1, [3] = 4 },
+			})
+			assert.same({
+				{
+					[1] = { [1] = 0, [2] = 5, [3] = 2, [4] = 8 },
+					[2] = { [1] = -3, [2] = 0, [3] = -2, [4] = 3 },
+					[3] = { [1] = -1, [2] = 4, [3] = 0, [4] = 7 },
+					[4] = { [1] = 1, [2] = 6, [3] = 3, [4] = 0 },
+				},
+				{
+					[1] = { [2] = 1, [3] = 1, [4] = 2 },
+					[2] = { [1] = 3, [3] = 2, [4] = 2 },
+					[3] = { [1] = 3, [2] = 1, [4] = 2 },
+					[4] = { [1] = 4, [2] = 1, [3] = 1 },
+				},
+			}, { g[name](g) })
+		end)
+		test_apsp_pos_weights(name)
+	end
+	describe("APSP", function()
+		describe("Floyd-Warshall", function()
+			test_apsp("apsp_floyd_warshall")
+		end)
+		describe("Bellman-Ford", function()
+			test_apsp("apsp_bellman_ford")
+			it("matches Floyd-Warshall", function()
+				local g = random_directed_acyclic_graph(function()
+					return math.random(-30, 30)
+				end)
+				local dist_bellman_ford = check_shortest_path_forest(g, g:apsp_bellman_ford())
+				local dist_floyd_warshall = check_shortest_path_forest(g, g:apsp_floyd_warshall())
+				assert.same(dist_bellman_ford, dist_floyd_warshall)
+			end)
+		end)
+		describe("Dijkstra", function()
+			test_apsp_pos_weights("apsp_dijkstra")
+			-- Note: Implicitly also tests whether SSSP matches
+			it("matches Bellman-Ford", function()
+				for _ = 1, 10 do
+					local g = random_graph_pos_weights()
+					local dist_dijkstra = check_shortest_path_forest(g, g:apsp_dijkstra())
+					local dist_bellman_ford = check_shortest_path_forest(g, g:apsp_bellman_ford())
+					assert.same(dist_bellman_ford, dist_dijkstra)
+				end
+			end)
+		end)
+		describe("Dispatch", function()
+			test_apsp("apsp")
+		end)
+	end)
+	it("Max flow", function()
+		assert.same(
+			graph.new({
+				s = { a = 2, b = 2 },
+				a = { c = 1, d = 1 },
+				b = { d = 2 },
+				c = { t = 1 },
+				d = { t = 3 },
+				t = {},
+			}),
+			graph
+				.new({
+					s = { a = 3, b = 2 },
+					a = { c = 2, d = 1 },
+					b = { c = 1, d = 3 },
+					c = { t = 1 },
+					d = { t = 4 },
+					t = {},
+				})
+				:max_flow("s", "t")
+		)
+	end)
+end)
---
+++ src/data_structures/graph.lua
@@ -0,0 +1,489 @@
+-- (Directed) graph using a table of tables data structure,
+-- allowing for amortized average case constant time insertion, deletion & traversal
+
+local table_heap = require("data_structures.table_heap")
+
+local graph = {}
+
+function graph.new(nodes)
+	return { _nodes = nodes or {} } -- nodes[from][to] = weight
+end
+
+function graph:add_node(node)
+	assert(self._nodes[node] == nil, "node already exists")
+	self._nodes[node] = {}
+end
+
+-- `remove_node` is intentionally omitted for now, since it would either
+-- require a reverse mapping to be efficient,
+-- or have to be implemented inefficiently using a scan over all edges.
+
+function graph:has_node(node)
+	return self._nodes[node] ~= nil
+end
+
+-- Set the weight of an edge. Overrides a previous weight.
+-- `from` and `to` must have already been added.
+function graph:set_weight(
+	from, -- node
+	to, -- node
+	weight -- weight of the edge, `nil` to remove; use `true` if there is no `weight`
+)
+	assert(self._nodes[to], "destination node missing")
+	assert(self._nodes[from], "source node missing")[to] = weight
+end
+
+-- Get the weight of an edge or `nil` if there is no edge from `from` to `to`
+function graph:get_weight(
+	from, --[[node]]
+	to --[[node]]
+)
+	return self._nodes[from] and self._nodes[from][to]
+end
+
+--> boolean, whether there is an edge between the two nodes
+function graph:has_edge(
+	from, --[[node]]
+	to --[[node]]
+)
+	return self:get_weight(from, to) ~= nil
+end
+
+-- Adds an edge with "weight" `true`. May overwrite the weight of an existing edge.
+function graph:add_edge(
+	from, --[[node]]
+	to --[[node]]
+)
+	return self:set_weight(from, to, true)
+end
+
+-- Removes an edge. No-op if there was no edge.
+function graph:remove_edge(
+	from, --[[node]]
+	to --[[node]]
+)
+	return self:set_weight(from, to, nil)
+end
+
+-- Copies a graph, copying the structure but not copying the nodes or weights as a deepcopy would.
+--> Copy of `self`
+function graph:copy()
+	local nodes_copy = {}
+	for node, neighbors in pairs(self._nodes) do
+		local neighbors_copy = {}
+		for neighbor, weight in pairs(neighbors) do
+			neighbors_copy[neighbor] = weight
+		end
+		nodes_copy[node] = neighbors_copy
+	end
+	return graph.new(nodes_copy)
+end
+
+local function next_key(t, k)
+	return (next(t, k))
+end
+
+function graph:nodes()
+	-- Iterator over the `node`s, in any order
+	return next_key, self._nodes
+end
+
+function graph:neighbors(
+	from --[[node]]
+)
+	-- Iterator over `to, weight`, in any order
+	return pairs(self._nodes[from])
+end
+
+function graph:edges()
+	-- Iterator over `from, to, weight`, in any order
+	return coroutine.wrap(function()
+		for from, tos in pairs(self._nodes) do
+			for to, weight in pairs(tos) do
+				coroutine.yield(from, to, weight)
+			end
+		end
+	end)
+end
+
+-- Breadth-first traversal. Can be used to solve shortest path problems if all edges have the same weight.
+function graph:nodes_breadth_first(
+	root --[[optional root node to start the traversal from]]
+)
+	local visited = {}
+	local function breadth_first_traversal(start)
+		visited[start] = true
+		local level = { start }
+		local depth = 0
+		coroutine.yield(start, depth, nil)
+		repeat
+			local next_level = {}
+			depth = depth + 1
+			for _, node in pairs(level) do
+				for neighbor in self:neighbors(node) do
+					if not visited[neighbor] then
+						coroutine.yield(neighbor, depth, node)
+						table.insert(next_level, neighbor)
+						visited[neighbor] = true
+					end
+				end
+			end
+			level = next_level
+		until level[1] == nil
+	end
+	-- Iterator over `node, hops, predecessor`, starting with `root, 0, nil`
+	return coroutine.wrap(function()
+		if root ~= nil then
+			assert(self._nodes[root])
+			return breadth_first_traversal(root)
+		end
+		for start in self:nodes() do
+			if not visited[start] then
+				breadth_first_traversal(start)
+			end
+		end
+	end)
+end
+
+-- Depth-first traversal
+function graph:nodes_depth_first(
+	root --[[optional root node to start the traversal from]]
+)
+	local visited = {}
+	local depth = 0
+	local function depth_first_traversal(node)
+		if visited[node] then
+			return
+		end
+		visited[node] = true
+		coroutine.yield(node, depth)
+		depth = depth + 1
+		for neighbor in self:neighbors(node) do
+			depth_first_traversal(neighbor)
+		end
+		depth = depth - 1
+	end
+	-- Iterator over `node, depth`, starting with `root, 0`
+	return coroutine.wrap(function()
+		if root ~= nil then
+			assert(self._nodes[root])
+			return depth_first_traversal(root)
+		end
+		for node in self:nodes() do
+			depth_first_traversal(node)
+		end
+	end)
+end
+
+-- Uses a DFS to determine whether `self` has a cycle.
+function graph:has_cycle()
+	local done = {} -- current state of each node
+	local function has_cycle(node)
+		if done[node] == false then
+			return true -- node is a successor of itself => cycle
+		end
+		if done[node] == true then
+			return false
+		end
+		done[node] = false
+		for neighbor in self:neighbors(node) do
+			if has_cycle(neighbor) then
+				return true
+			end
+		end
+		done[node] = true
+		return false
+	end
+	for node in self:nodes() do
+		if has_cycle(node) then
+			return true -- if `self` has a cycle consisting of edges satisfying `follow_edge`
+		end
+	end
+	return false -- if `self` has no cycle consisting of edges satisfying `follow_edge`
+end
+
+-- Topological sort implemented as reverse postorder.
+-- Requires a cycle-free graph.
+--> Stateful iterator returning nodes in topological order.
+function graph:nodes_topological_order()
+	-- Find roots (nodes without ingoing edges)
+	local roots = {}
+	for node in self:nodes() do
+		roots[node] = true
+	end
+	for _, to in self:edges() do
+		roots[to] = nil
+	end
+	local nodes = {}
+	local done = {}
+	local function topo_sort(node)
+		if done[node] then
+			return -- done with node
+		end
+		-- Re-encountered parent which is still in progress => cycle
+		assert(done[node] == nil, "graph contains cycle")
+		done[node] = false -- node is still in progress
+		for neighbor in self:neighbors(node) do
+			topo_sort(neighbor)
+		end
+		done[node] = true
+		table.insert(nodes, node)
+	end
+	-- Topo. sort each connected component (order among them is arbitrary though)
+	for root in pairs(roots) do
+		topo_sort(root)
+	end
+	-- Check that all nodes have been seen
+	for node in self:nodes() do
+		assert(done[node], "graph contains cycle")
+	end
+	-- Iterate over nodes in reverse order
+	local i = #nodes + 1
+	return function()
+		i = i - 1
+		return nodes[i] -- will be `nil` for `i < 1`
+	end
+end
+
+-- Single source shortest paths using Dijkstra.
+-- Requires positive weights.
+function graph:sssp_dijkstra(source)
+	local dist, predec = {}, {}
+	dist[source] = 0
+	local closest = table_heap.new({}, function(v, w)
+		return dist[v] < dist[w]
+	end)
+	closest:push(source)
+	repeat
+		local closest_node = closest:pop()
+		for neighbor, weight in self:neighbors(closest_node) do
+			assert(weight >= 0, "negative weight edge")
+			local candidate_dist = dist[closest_node] + weight
+			if dist[neighbor] == nil then -- new node reached
+				dist[neighbor], predec[neighbor] = candidate_dist, closest_node
+				closest:push(neighbor)
+			elseif candidate_dist < dist[neighbor] then
+				dist[neighbor], predec[neighbor] = candidate_dist, closest_node
+				closest:decrease(neighbor)
+			end
+		end
+	until closest:top() == nil
+	return dist, predec -- unreachable nodes will have `dist[node] = predec[node] = nil`
+end
+
+-- Single source shortest paths using Bellman-Ford.
+-- Supports negative weights.
+function graph:sssp_bellman_ford(source)
+	local dist, predec = {}, {}
+	dist[source] = 0
+	for _ in next, self._nodes, next(self._nodes) do -- skip the first node => |nodes| - 1 iterations
+		for from, to, weight in self:edges() do
+			if dist[from] then -- from is reachable?
+				local candidate_dist = dist[from] + weight
+				-- Try relaxing the distance to `to`
+				if dist[to] == nil or candidate_dist < dist[to] then
+					-- `to` is currently unreachable or using `from` as predecessor is shorter
+					dist[to], predec[to] = candidate_dist, from
+				end
+			end
+		end
+	end
+	-- Final iteration to check for a negative weight cycle
+	for from, to, weight in self:edges() do
+		if dist[from] then
+			local candidate_dist = dist[from] + weight
+			if dist[to] == nil or candidate_dist < dist[to] then
+				error("negative weight cycle")
+			end
+		end
+	end
+	return dist, predec -- unreachable nodes will have `dist[node] = predec[node] = nil`
+end
+
+-- Single source shortest paths using either Bellman-Ford or Dijkstra
+-- depending on whether the graph contains negative weights
+--> `dist, predec` such that
+--> * `dist[from][to]` is the distance between the nodes `from` and `to` or `nil` if `to` is unreachable from `from`
+--> * `predec[from][to]` is the predecessor of `to` on the shortest path from `from` or `nil` if `to` is unreachable
+function graph:sssp(
+	source --[[node, start of all shortest paths]]
+)
+	for _, _, weight in self:edges() do
+		if weight < 0 then
+			return self:sssp_bellman_ford(source)
+		end
+	end
+	return self:sssp_dijkstra(source)
+end
+
+-- All pair shortest paths using single source shortest paths
+local function _apsp_sssp(self, sssp_name)
+	local dist, predec = {}, {}
+	for node in self:nodes() do
+		dist[node], predec[node] = self[sssp_name](self, node)
+	end
+	return dist, predec
+end
+
+-- All pair shortest paths using Dijkstra.
+-- Requires positive weights. Suitable for "sparse" graphs.
+function graph:apsp_dijkstra()
+	return _apsp_sssp(self, "sssp_dijkstra")
+end
+
+-- All pair shortest paths using Bellman-Ford.
+-- Allows negative weights. Rarely suitable (only for *very* sparse graphs).
+function graph:apsp_bellman_ford()
+	return _apsp_sssp(self, "sssp_bellman_ford")
+end
+
+-- All pair shortest paths using Floyd-Warshall.
+-- Allows negative weights. Detects negative weight cycles.
+-- Go-to choice for APSP.
+function graph:apsp_floyd_warshall()
+	local dist, predec = {}, {}
+	for from in self:nodes() do
+		dist[from], predec[from] = {}, {}
+		for to, weight in self:neighbors(from) do
+			dist[from][to], predec[from][to] = weight, from
+		end
+		dist[from][from] = 0
+	end
+	for over in self:nodes() do
+		local next_dist = {}
+		for from in self:nodes() do
+			next_dist[from] = {}
+			for to in self:nodes() do
+				next_dist[from][to] = dist[from][to]
+				-- Try relaxing
+				if dist[from][over] and dist[over][to] then
+					local dist_over = dist[from][over] + dist[over][to]
+					if (not dist[from][to]) or dist_over < dist[from][to] then
+						next_dist[from][to] = dist_over
+						predec[from][to] = over
+					end
+				end
+			end
+		end
+		dist = next_dist
+	end
+	for node in self:nodes() do
+		assert(dist[node][node] == 0, "negative weight cycle")
+	end
+	return dist, predec
+end
+
+-- All pairs shortest paths.
+-- Uses either Dijkstra, Bellman-Ford or Floyd-Warshall
+-- depending on how sparse the graph is and whether negative weights occur.
+--> `dist, predec` such that
+--> * `dist[from][to]` is the distance between the nodes `from` and `to` or `nil` if `to` is unreachable from `from`
+--> * `predec[from][to]` is the predecessor of `to` on the shortest path from `from` or `nil` if `to` is unreachable
+function graph:apsp()
+	-- TODO (...) Introduce constant factor(s) here after profiling
+	local nodes = 0
+	for _ in self:nodes() do
+		nodes = nodes + 1
+	end
+	local edges = 0
+	local negative_weight = false
+	for _, _, weight in self:edges() do
+		edges = edges + 1
+		if weight < 0 then
+			negative_weight = true
+		end
+	end
+	if negative_weight then -- Dijkstra is not an option
+		if edges >= nodes then
+			-- nodes^2 * edges >= nodes^3 => Floyd-Warshall ">=" Bellman-Ford
+			return self:apsp_floyd_warshall()
+		end
+		return self:apsp_bellman_ford()
+	end
+	-- Expected running times divided by the common factor of the amount of nodes
+	local time_dijkstra = (nodes + edges) * math.log(nodes)
+	local time_floyd_warshall = nodes ^ 2
+	if time_floyd_warshall <= time_dijkstra then
+		return self:apsp_floyd_warshall()
+	end
+	local time_bellman_ford = nodes * edges
+	-- For *extremely* sparse graphs Bellman-Ford is faster; it's also simpler
+	if time_bellman_ford <= time_dijkstra then
+		return self:apsp_bellman_ford()
+	end
+	return self:apsp_dijkstra()
+end
+
+-- All pairs shortest paths if `source == nil`, single source shortest paths otherwise.
+function graph:shortest_paths(
+	source -- source node (for SSSP) or `nil` (for APSP)
+)
+	if source == nil then
+		return self:apsp()
+	end
+	return self:sssp(source)
+end
+
+-- Uses Edmonds-Karp. Weights are interpreted as capacities.
+--> Flow graph. Does not contain edges with `0` flow:
+--> * Source set S can be found as everything reachable from source (e.g. using DFS/BFS).
+--> * Sink set T can be found as all nodes not in S.
+--> * Min-cut can be found as the set of all edges from S to T.
+function graph:max_flow(source, sink)
+	local residual = self:copy()
+	-- Calculates the bottleneck along a path given as a predecessor chain from sink to source
+	local function calc_max_augment(predec)
+		local max_augment = math.huge -- min. of weights along the path
+		local to = sink
+		repeat
+			local from = predec[to]
+			max_augment = math.min(max_augment, residual:get_weight(from, to))
+			to = from
+		until to == source
+		return max_augment
+	end
+	-- Augments a given path given as a predecessor chain from sink to source
+	local function augment_path(predec)
+		local max_augment = calc_max_augment(predec)
+		local to = sink
+		repeat
+			local from = predec[to]
+			residual:set_weight(from, to, residual:get_weight(from, to) - max_augment) -- reduce capacity
+			residual:set_weight(to, from, (residual:get_weight(to, from) or 0) + max_augment) -- increase capacity of back edge
+			to = from
+		until to == source
+	end
+	-- Tries augmenting a path from source to sink.
+	-- Uses a BFS to try paths with fewer edges first.
+	-- Recurses until there is no path from source to sink in the residual network.
+	local function try_augment_path()
+		local level = { source }
+		local predec = { [source] = source } -- HACK to prevent the source from being "discovered"
+		repeat
+			local next_level = {}
+			for _, node in ipairs(level) do
+				for neighbor, capacity in residual:neighbors(node) do
+					if capacity > 0 and predec[neighbor] == nil then -- TODO remove edges instead?
+						predec[neighbor] = node
+						if neighbor == sink then -- path to sink found
+							augment_path(predec)
+							return try_augment_path()
+						end
+						table.insert(next_level, neighbor)
+					end
+				end
+			end
+			level = next_level
+		until #level == 0
+	end
+	try_augment_path()
+	-- Reconstruct the flow from the final residual network.
+	local flow = self:copy()
+	for from, to in flow:edges() do
+		-- Note: Works as expected if weight from `to` to `from` is `nil` in the residual network
+		flow:set_weight(from, to, residual:get_weight(to, from))
+	end
+	return flow
+end
+
+return require("class")(graph)
