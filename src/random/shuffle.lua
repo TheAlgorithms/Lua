5826b380fe0318ddee79554864ca144e38084d56
Lars Müller <34514239+appgurueu@users.noreply.github.com>
2023-09-09 22:58:10 +0200

    Clean up variable names in `random.shuffle`

--- src/random/shuffle.lua
+++ src/random/shuffle.lua
@@ -2,8 +2,8 @@
 return function(
 	list -- list to be shuffled in-place
 )
-	for index = 1, #list - 1 do
-		local index_2 = math.random(index, #list)
-		list[index], list[index_2] = list[index_2], list[index]
+	for i = 1, #list - 1 do
+		local j = math.random(i, #list)
+		list[i], list[j] = list[j], list[i]
 	end
 end
