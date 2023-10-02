d7064aea0938794b07c7f6017005cfec575fbab2
Lars Müller <34514239+appgurueu@users.noreply.github.com>
2023-10-02 18:49:15 +0200

    Use tabs for formatting, move doc comment to proper place

--- src/math/totient.lua
+++ src/math/totient.lua
@@ -1,15 +1,13 @@
--- The totient function returns the number of positive integers less than n that are relatively prime to n
-
 local gcd = require("math.greatest_common_divisor")
 
 return function(
-  n -- number
+	n -- number
 )
-  local result = 0
-  for i = 1, n do
-    if gcd(i, n) == 1 then -- check if relatively prime
-      result = result + 1
-    end
-  end
-  return result
+	local totient = 0
+	for i = 1, n do
+    	if gcd(i, n) == 1 then -- relatively prime?
+			totient = totient + 1
+		end
+	end
+	return totient -- number of positive integers less than n that are relatively prime to n
 end
