e63ae69c9f60012b8aa2db9b84f2e3d1b5fdf091
vil02 <vil02@o2.pl>
2023-11-10 17:37:58 +0100

    style: run `stylua` on `.spec`

--- .spec/math/simplist_radical_form_spec.lua
+++ .spec/math/simplist_radical_form_spec.lua
@@ -1,7 +1,7 @@
 describe("Simplist radical form", function()
 	local srf = require("math.simplist_radical_form")
 	local function test_srf(x, expected_coeff, expected_root_term)
-		assert.same({expected_coeff, expected_root_term}, {srf(x)})
+		assert.same({ expected_coeff, expected_root_term }, { srf(x) })
 	end
 	it("handles edge cases properly", function()
 		test_srf(0, 1, 0)
@@ -9,14 +9,14 @@ describe("Simplist radical form", function()
 	end)
 	it("works for square numbers", function()
 		for n = 1, 1e3 do
-			test_srf(n*n, n, 1)
+			test_srf(n * n, n, 1)
 		end
 	end)
 	it("works for products of primes ppq", function()
-		local primes = {1, 3, 7, 11, 1013, 4903, 7919}
+		local primes = { 1, 3, 7, 11, 1013, 4903, 7919 }
 		for _, p in ipairs(primes) do
 			for _, q in ipairs(primes) do
-				test_srf(p*p*q, p, q)
+				test_srf(p * p * q, p, q)
 			end
 		end
 	end)
