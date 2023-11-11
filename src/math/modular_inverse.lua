f00270b6d7bf4eae8de31c85f484d454240e84d9
Lars Müller <34514239+appgurueu@users.noreply.github.com>
2023-11-11 11:32:24 +0100

    Some cleanup

--- src/math/modular_inverse.lua
+++ src/math/modular_inverse.lua
@@ -1,17 +1,16 @@
---- computes the inverse of a modulo m, i.e.
---- finds number x such that
---- (a * x) % m == 1 and 0 < x < m
+local extended_gcd = require("math.greatest_common_divisor")
+
+-- Computes the inverse of `a` modulo `m`, i.e.
+-- finds a number `x` such that
+-- (a * x) % m == 1 and 0 < x < m
 return function(
 	a, -- number
 	m -- modulus
 )
-	if m <= 0 then
-		error("Modulus m = " .. m .. " must be positive.")
-	end
-	local gcd_fun = require("math.greatest_common_divisor")
-	local gcd, x, _ = gcd_fun(a % m, m)
+	assert(m > 0, "modulus must be positive")
+	local gcd, x, _ = extended_gcd(a % m, m)
 	if a ~= 0 and gcd == 1 then
-		--- enure that result is in (0, m)
+		-- Ensure that result is in (0, m)
 		return x % m
 	end
 	return nil
