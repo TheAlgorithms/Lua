0428a2a4fa5125ba3d32cbff738140691d60ff1d
Piotr Idzik <65706193+vil02@users.noreply.github.com>
2023-11-11 11:33:45 +0100

    Add modular inverse (#13)

---
+++ src/math/modular_inverse.lua
@@ -0,0 +1,17 @@
+local extended_gcd = require("math.greatest_common_divisor")
+
+-- Computes the inverse of `a` modulo `m`, i.e.
+-- finds a number `x` such that
+-- (a * x) % m == 1 and 0 < x < m
+return function(
+	a, -- number
+	m -- modulus
+)
+	assert(m > 0, "modulus must be positive")
+	local gcd, x, _ = extended_gcd(a % m, m)
+	if a ~= 0 and gcd == 1 then
+		-- Ensure that result is in (0, m)
+		return x % m
+	end
+	return nil
+end
