b3af65bec9c7cfc65e123e57ff1e2bdeb300256b
Lars Müller <34514239+appgurueu@users.noreply.github.com>
2023-11-17 00:22:52 +0100

    Fix typos

--- src/math/solve_simultaneous_congruences.lua
+++ src/math/solve_simultaneous_congruences.lua
@@ -16,12 +16,12 @@ end
 -- ```
 -- where `a_0`, `a_1`, ... and `m_0`, `m_1`, ... are given.
 --
--- The system is represented by list of pairs.
--- The pair `{a, m}` represents a congruance `x = a mod m`.
--- For the input {{a_0, m_0}, {a_1, m_1}, ...},
+-- The system is represented by a list of pairs.
+-- The pair `{a, m}` represents a congruence `x = a mod m`.
+-- For the input `{{a_0, m_0}, {a_1, m_1}, ...}`,
 -- it finds a number `x`, such that
 -- `x = a_i mod m_i` and `0 < x < m_0 * m_1 * ...`.
--- The imlementation is based on the Chinese remainder theorem.
+-- The implementation is based on the Chinese remainder theorem.
 return function(congruences)
 	local all_prod = all_moduli_prod(congruences)
 
@@ -32,7 +32,7 @@ return function(congruences)
 		local cur_prod = math.floor(all_prod / modulus)
 		local cur_inv = modular_inverse(cur_prod, modulus)
 		if cur_inv == nil then
-			-- moduli of the congruances are not co-prime
+			-- moduli of the congruences are not co-prime
 			return nil
 		end
 		res = (res + residue * cur_inv * cur_prod) % all_prod
