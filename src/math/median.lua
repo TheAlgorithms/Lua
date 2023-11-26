bb6f0752fe1522f4cd448c92c15c3aed6715899c
Lars Müller <34514239+appgurueu@users.noreply.github.com>
2023-11-26 22:04:04 +0100

    Copy using loop, remove redundant `else`

--- src/math/median.lua
+++ src/math/median.lua
@@ -8,12 +8,14 @@ return function(nums)
 	if #nums == 1 then
 		return nums[1]
 	end
-	local copy = { unpack(nums) }
+	local copy = {}
+	for i = 1, #nums do
+		copy[i] = nums[i]
+	end
 	local mid_pos = quickselect(copy, math.ceil(#copy / 2))
 	if #nums % 2 == 1 then
 		return copy[mid_pos]
-	else
-		local next_mid_pos = quickselect(copy, 1, nil, mid_pos + 1)
-		return (copy[mid_pos] + copy[next_mid_pos]) / 2
 	end
+	local next_mid_pos = quickselect(copy, 1, nil, mid_pos + 1)
+	return (copy[mid_pos] + copy[next_mid_pos]) / 2
 end
